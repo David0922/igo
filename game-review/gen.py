773d3352e185f3f9c4e0098feb26b5569d6b9fe1
David Chen <david0x0922@gmail.com>
2025-03-08 19:12:15 +0800

    -

--- game-review/gen.py
+++ game-review/gen.py
@@ -1,20 +1,15 @@
 import os
-from collections import defaultdict
 
 
 def main():
-  sgf = defaultdict(list)
   for dir, _, files in os.walk(os.getcwd()):
-    for f in files:
-      if f[-4:] == '.sgf':
-        sgf[dir].append(f)
-  for dir, sgfs in sgf.items():
-    sub_dir = dir.split('game-review/')[-1]
-    sgfs.sort()
-    with open(f'{dir}/README.md', 'w+') as md:
-      md.write('\n\n'.join(
-        f'[{f}](https://raw.githubusercontent.com/David0922/igo/refs/heads/main/game-review/{sub_dir}/{f})'
-        for f in sgfs))
+    sgfs = sorted([f for f in files if f[-4:] == '.sgf'])
+    if sgfs:
+      sub_dir = dir.split('game-review/')[-1]
+      with open(f'{dir}/README.md', 'w') as md:
+        md.write('\n\n'.join(
+          f'[{f}](https://raw.githubusercontent.com/David0922/igo/refs/heads/main/game-review/{sub_dir}/{f})'
+          for f in sgfs))
 
 
 if __name__ == '__main__':
